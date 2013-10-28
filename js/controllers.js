55c2b19b42efb5eb582c7b43ab27f3a27765c810
Antonio Jesús Sánchez Padial <antonio@tyba.com>
2013-10-28 21:55:58 +0100

    First draft gh-pages.

---
+++ js/controllers.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('MyCtrl1', function() {
+	$scope.sounds = [
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'}
+	];
+  })
+  .controller('MyCtrl2', [function() {
+
+  }]);

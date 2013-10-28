26ad41f2c518df0e846d044c772084364ab3f4aa
Antonio Jesús Sánchez Padial <antonio@tyba.com>
2013-10-28 21:52:34 +0100

    First controller drawind thumbnails.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,22 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
+  controller('MyCtrl1', function() {
+	$scope.sounds = [
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
+		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'}
+	];
+  })
   .controller('MyCtrl2', [function() {
 
   }]);

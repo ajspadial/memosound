989063c101b3a23dee1f0129e65326b0f65416a4
Antonio Jesús Sánchez Padial <antonio@tyba.com>
2013-10-28 21:59:50 +0100

    Added  to controller1

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', function() {
+  controller('MyCtrl1', function($scope) {
 	$scope.sounds = [
 		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
 		{pic: 'http://osxdaily.com/wp-content/uploads/2013/03/sound-icon.png'},
